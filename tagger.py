9e18b4e99f3f6f8a327d777290c7599d07e91261
yorya11 <31896153+yorya11@users.noreply.github.com>
2017-10-25 20:32:10 +0300

    Add files via upload

--- tagger.py
+++ tagger.py
@@ -1,21 +1,39 @@
 import sys
 
-frequent_tag = 'x'
-count = 0
 fd = open('model.tsv')
 
+frequent_tag = 'x'
+last_max = 0
+
+word_tag = {}
+word_prob = {}
+
 for line in fd.readlines():
-	line = line.strip('\n')
+	line = line.strip()
 	line = line.split('\t')
-	if int(line[1]) > count:
-		count = int(line[1])
+	if int(line[1]) > last_max:
+		last_max = int(line[1])
 		frequent_tag = line[2]
-print(frequent_tag)
+	surface_form = line[3]
+	prob = float(line[0])
+	tag = line[2]
+	if surface_form not in word_tag:
+		word_tag[surface_form] = tag
+		word_prob[surface_form] = prob
+	if surface_form in word_tag:
+		if prob > word_prob[surface_form]:
+			word_tag[surface_form] = tag
+			word_prob[surface_form] = prob
+
 input = sys.stdin.readlines()
 for line in input:
 	line = line.strip()
 	if '\t' not in line:
-		continue 
-	row = line.split('\t')
-	row[3] = frequent_tag
-	print('\t', join(row))
+		print(line)
+		continue
+	line = line.split('\t')
+	if line[1] in word_tag:
+		line[3] = word_tag[line[1]]
+	else:
+		line[3] = frequent_tag
+	print('\t'.join(line))
